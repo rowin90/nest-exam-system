e27d564459bfe0b565416e3ea5edb3e8cbb8ecf9
饶驹 <raoju1580@qq.com>
2024-09-10 18:56:58 +0800

    ✨ feat: 新增 prisma orm 集成

--- apps/user/src/user.controller.ts
+++ apps/user/src/user.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Body, Post } from '@nestjs/common';
 import { UserService } from './user.service';
+import { RegisterUserDto } from './dto/register-user.dto';
 
-@Controller()
+@Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
   async getHello(): Promise<string> {
     return await this.userService.getHello();
+  }
+
+  @Post('register')
+  async register(@Body() registerUser: RegisterUserDto) {
+    delete registerUser.captcha;
+    return await this.userService.create(registerUser);
   }
 }
--- apps/user/src/user.service.ts
+++ apps/user/src/user.service.ts
@@ -1,12 +1,26 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { RedisService } from '@app/redis';
+import { PrismaService } from '@app/prisma';
+import { Prisma } from '@prisma/client';
 @Injectable()
 export class UserService {
   @Inject(RedisService)
   redisService: RedisService;
 
+  @Inject(PrismaService)
+  prisma: PrismaService;
+
   async getHello(): Promise<string> {
     const keys = await this.redisService.keys('*');
     return 'Hello World!' + keys;
+  }
+
+  async create(data: Prisma.UserCreateInput) {
+    return await this.prisma.user.create({
+      data,
+      select: {
+        id: true,
+      },
+    });
   }
 }
